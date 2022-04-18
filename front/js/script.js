1aeec6a6c83e6b1625ca79e5458f111789b6059e
yeeahG <yeji.sep.k@gmail.com>
2022-04-18 23:13:06 +0900

    [User]user account

--- front/js/script.js
+++ front/js/script.js
@@ -1,5 +1,24 @@
 let navbar = document.querySelector('.header .flex .navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
+document.querySelector('#menu-btn').onclick = () => {
    navbar.classList.toggle('active');
+}
+
+/*user*/
+let account = document.querySelector('.user-account');
+
+document.querySelector('#user-btn').onclick = () => {
+   account.classList.toggle('active');
+}
+
+document.querySelector('#close-account').onclick = () => {
+    account.classList.remove('active');
+}
+
+
+
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+
 }
